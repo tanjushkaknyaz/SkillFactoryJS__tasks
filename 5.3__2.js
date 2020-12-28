8b529598ea27172682d4df72f46230c7c354df37
tanjushkaknyaz <73173955+tanjushkaknyaz@users.noreply.github.com>
2020-12-28 15:24:04 +0300

    Add files via upload
    5.3__2 correction

--- 5.3__2.js
+++ 5.3__2.js
@@ -1,21 +1,23 @@
-let x = prompt("Пожалуйста, введите Х");
-let type = typeof x;
-if (x == NaN) {
-  console.log(`Тип Х не определен`);
-}
-break;
-console.log(type);
-switch (type) {
-  case "number":
-    console.log(`${x} - Number`);
-    break;
-  case "boolean":
-    console.log(`${x} - Boolean`);
-    break;
-  case "string":
-    console.log(`${x}String`);
-    break;
-  default:
+let x = 1;
+function checkType(x) {
+  let type = typeof x;
+  if (isNaN(x) && type != "string" && type != "boolean") {
     console.log(`Тип Х не определен`);
-    break;
+  } else {
+    switch (type) {
+      case "number":
+        console.log(`${x} - Number`);
+        break;
+      case "boolean":
+        console.log(`${x} - Boolean`);
+        break;
+      case "string":
+        console.log(`${x} - String`);
+        break;
+      default:
+        console.log(`Тип Х не определен`);
+        break;
+    }
+  }
 }
+checkType(x);
